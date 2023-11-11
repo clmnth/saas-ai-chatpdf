20ef8a99dd2d662b9654ff8f8ad6a68cca18bdfa
Clmt <huvigclement@gmail.com>
2023-11-11 16:45:19 +0700

    landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,13 @@
-import Image from 'next/image'
+import MaxWidthWrapper from "@/components/MaxWidthWrapper";
 
 export default function Home() {
   return (
-    <div>Hello, World!</div>
-  )
+    <MaxWidthWrapper className="mb-12 mt-28 sm:mt-40 flex flex-col items-center justify-center">
+      <div className="mx-auto mb-4 flex max-w-fit items-center justify-center space-x-2 overflow-hidden rounded-full border-gray-200 bg-white px-7 py-2 shadow-md backdrop-blur transition-all hover:border-gray-300 hover:bg-white/50">
+        <p className="text-sm font-semibold text-gray-700">
+          ChatPDF is now public!
+        </p>
+      </div>
+    </MaxWidthWrapper>
+  );
 }
