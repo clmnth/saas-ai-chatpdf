1d66a010fb74199896b607b706cba0257c14e084
Clmt <huvigclement@gmail.com>
2023-11-13 17:59:00 +0700

    landing page headline and  button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,35 @@
 import MaxWidthWrapper from "@/components/MaxWidthWrapper";
+import Link from "next/link";
+import { ArrowRight } from "lucide-react";
+import { buttonVariants } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <MaxWidthWrapper className="mb-12 mt-28 sm:mt-40 flex flex-col items-center justify-center">
-      <div className="mx-auto mb-4 flex max-w-fit items-center justify-center space-x-2 overflow-hidden rounded-full border-gray-200 bg-white px-7 py-2 shadow-md backdrop-blur transition-all hover:border-gray-300 hover:bg-white/50">
+    <MaxWidthWrapper className="mb-12 mt-28 sm:mt-40 flex flex-col items-center justify-center text-center">
+      <div className="mx-auto mb-4 flex max-w-fit items-center justify-center space-x-2 overflow-hidden rounded-full border border-gray-200 bg-white px-7 py-2 shadow-md backdrop-blur transition-all hover:border-gray-300 hover:bg-white/50">
         <p className="text-sm font-semibold text-gray-700">
           ChatPDF is now public!
         </p>
       </div>
+      <h1 className="max-w-4xl text-5xl font-bold md:text-6xl lg:text-7xl">
+        Chat with your <span className="text-red-600">documents</span> in
+        seconds.
+      </h1>
+      <p className="mt-5 max-w-prose text-zinc-700 sm:text-lg">
+        ChatPDF allows you to have conversations with any PDF document. Simply
+        upload your file and start asking questions right away.
+      </p>
+
+      <Link
+        className={buttonVariants({
+          size: "lg",
+          className: "mt-5",
+        })}
+        href="/dashboard"
+        target="_blank"
+      >
+        Get started <ArrowRight className="ml-2 h-5 w-5" />
+      </Link>
     </MaxWidthWrapper>
   );
 }
