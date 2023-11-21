da43b6af9433b329b59c01ebaa38621f869b6692
Clmt <huvigclement@gmail.com>
2023-11-21 16:55:20 +0700

    tRPC setup #2

--- src/trpc/index.ts
+++ src/trpc/index.ts
@@ -1,8 +1,19 @@
-import { router } from "./trpc"
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import { publicProcedure, router } from "./trpc";
+import { TRPCError } from "@trpc/server";
 
 export const appRouter = router({
-  // ...
-});
+  authCallback: publicProcedure.query(async () => {
+    const { getUser } = getKindeServerSession()
+    const user = await getUser()
 
+    if (!user.id || !user.email)
+      throw new TRPCError({ code: "UNAUTHORIZED"})
+
+      // check if the user is in the database
+
+
+  }),
+});
 
 export type AppRouter = typeof appRouter;
