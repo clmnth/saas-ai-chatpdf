d91818f791fa004774483d015bce85abd778e883
Clmt <huvigclement@gmail.com>
2023-11-28 16:51:54 +0700

    pdf product page design

--- src/app/dashboard/[fileid]/page.tsx
+++ src/app/dashboard/[fileid]/page.tsx
@@ -1,3 +1,5 @@
+import ChatWrapper from "@/components/ChatWrapper";
+import PdfRenderer from "@/components/PdfRenderer";
 import { db } from "@/db";
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import { notFound, redirect } from "next/navigation";
@@ -10,24 +12,38 @@ interface PageProps {
 
 const Page = async ({ params }: PageProps) => {
   // retrieve the file id
-   const {fileid} = params
+  const { fileid } = params;
 
-  const {getUser} = getKindeServerSession();
+  const { getUser } = getKindeServerSession();
   const user = await getUser();
 
-  if (!user || !user.id) redirect(`/auth-callback?origin=dashboard/$(fileid)`)
+  if (!user || !user.id) redirect(`/auth-callback?origin=dashboard/$(fileid)`);
 
   // make database call
   const file = await db.file.findFirst({
-   where: {
+    where: {
       id: fileid,
-      userId: user.id
-   }
-  })
+      userId: user.id,
+    },
+  });
 
-  if(!file) notFound()
+  if (!file) notFound();
 
-  return <div>{fileid}</div>;
+  return (
+    <div className="flex-1 justify-between flex flex-col h-[calc(100vh-3.5rem)]">
+      <div className="mx-auto w-full mx-w-8xl grow lg:flex xl:px-2">
+         {/* left side*/}
+         <div className="flex-1 xl:flex">
+            <div className="px-4 py-6 sm:px-6 lg:pl-8 xl:flex-1 xl:pl-6">
+               <PdfRenderer />
+            </div>
+         </div>
+         <div className="shrink-0 flex-[0.75] border-t border-gray-200 lg:w-96 lg:border-l lg:border-t-0">
+            <ChatWrapper />
+         </div>
+      </div>
+    </div>
+  );
 };
 
 export default Page;
