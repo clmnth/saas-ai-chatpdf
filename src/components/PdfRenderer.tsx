030945b5588840648009bfc70cb27ef96ffbc2d1
Clmt <huvigclement@gmail.com>
2023-11-29 16:05:55 +0700

    file uploader UI

--- src/components/PdfRenderer.tsx
+++ src/components/PdfRenderer.tsx
@@ -2,12 +2,10 @@ const PdfRenderer = () => {
   return (
     <div className="w-full bg-white rounded-md shadow flex flex-col items-center">
       <div className="h-14 w-full border-b border-zinc-200 flex items-center justify-between px-2">
-         <div className="flex items-center gap-1.5">
-Top bar
-         </div>
+        <div className="flex items-center gap-1.5">Top bar</div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default PdfRenderer
+export default PdfRenderer;
--- src/components/UploadButton.tsx
+++ src/components/UploadButton.tsx
@@ -3,6 +3,90 @@
 import { useState } from "react";
 import { Dialog, DialogContent, DialogTrigger } from "./ui/dialog";
 import { Button } from "./ui/button";
+import { Cloud, File } from "lucide-react";
+
+import DropZone from "react-dropzone";
+import { Progress } from "./ui/progress";
+
+const UploadDropZone = () => {
+
+  const [isUploading, setIsUploading] = useState<boolean>(true);
+  const [uploadProgress, setUploadProgress] = useState<number>(0);
+
+  const startSimulatedProgress = () => {
+    setUploadProgress(0)
+
+    const interval = setInterval(() => {
+      setUploadProgress((prevProgress) => {
+        if(prevProgress >= 95) {
+          clearInterval(interval)
+          return prevProgress
+        }
+        return prevProgress + 5
+      })
+    }, 500)
+
+    return interval
+  }
+
+
+  return (
+    <DropZone
+      multiple={false}
+      onDrop={async (acceptedFile) => {
+        setIsUploading(true)
+
+        const progressInterval = startSimulatedProgress()
+
+        // handle file uploading
+       
+        clearInterval(progressInterval);
+        setUploadProgress(100);
+
+      }}
+    >
+      {({ getRootProps, getInputProps, acceptedFiles }) => (
+        <div
+          {...getRootProps()}
+          className="border h-64 m-4 border-dashed border-gray-300 rounded-lg"
+        >
+          <div className="flex items-center justify-center h-full w-full">
+            <label
+              htmlFor="dropzone-file"
+              className="flex flex-col items-center justify-center w-full h-full rounded-lg cursor-pointer bg-gray-50 hover:bg-gray-100"
+            >
+              <div className="flex flex-col items-center justify-center pt-5 pb-6">
+                <Cloud className="h-6 w-6 text-zinc-500 mb-2" />
+                <p className="mb-2 text-sm text-zinc-700">
+                  <span className="font-semibold">Click to upload</span> or drag
+                  and drop
+                </p>
+                <p className="text-xs text-zinc-500">PDF (up to 4MB)</p>
+              </div>
+              {/* user feedback when file uploaded */}
+              {acceptedFiles && acceptedFiles[0] ? (
+                <div className="max-w-xs bg-white flex items-center rounded-md overflow-hidden outline-[1px] outline-zinc-200 divide-x divide-zinc-200">
+                  <div className="px-3 py-2 h-full grid place-items-center">
+                    <File className="h-4 w-4 text-red-500" />
+                  </div>
+                  <div className="px-3 py-2 h-full text-sm truncate">
+                    {acceptedFiles[0].name}
+                  </div>
+                </div>
+              ) : null}
+
+              {isUploading ? (
+                <div className="w-full mt-4 max-w-xs mx-auto">
+                  <Progress value={uploadProgress } className="h-1 w-full bg-zinc-200"/>
+                </div>
+              ) : null}
+            </label>
+          </div>
+        </div>
+      )}
+    </DropZone>
+  );
+};
 
 const UploadButton = () => {
   const [isOpen, setIsOpen] = useState<boolean>(false);
@@ -20,7 +104,7 @@ const UploadButton = () => {
         <Button>Upload PDF</Button>
       </DialogTrigger>
       <DialogContent>
-         example content
+        <UploadDropZone />
       </DialogContent>
     </Dialog>
   );
