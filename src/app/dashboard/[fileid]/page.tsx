264ba138bb7f5c46253c22bdb07d96c2e5233df0
Clmt <huvigclement@gmail.com>
2023-12-01 16:51:44 +0700

    message section

--- src/app/dashboard/[fileid]/page.tsx
+++ src/app/dashboard/[fileid]/page.tsx
@@ -1,4 +1,4 @@
-import ChatWrapper from "@/components/ChatWrapper";
+import ChatWrapper from "@/components/chat/ChatWrapper";
 import PdfRenderer from "@/components/PdfRenderer";
 import { db } from "@/db";
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
@@ -32,15 +32,15 @@ const Page = async ({ params }: PageProps) => {
   return (
     <div className="flex-1 justify-between flex flex-col h-[calc(100vh-3.5rem)]">
       <div className="mx-auto w-full mx-w-8xl grow lg:flex xl:px-2">
-         {/* left side*/}
-         <div className="flex-1 xl:flex">
-            <div className="px-4 py-6 sm:px-6 lg:pl-8 xl:flex-1 xl:pl-6">
-               <PdfRenderer url={file.url}/>
-            </div>
-         </div>
-         <div className="shrink-0 flex-[0.75] border-t border-gray-200 lg:w-96 lg:border-l lg:border-t-0">
-            <ChatWrapper />
-         </div>
+        {/* left side*/}
+        <div className="flex-1 xl:flex">
+          <div className="px-4 py-6 sm:px-6 lg:pl-8 xl:flex-1 xl:pl-6">
+            <PdfRenderer url={file.url} />
+          </div>
+        </div>
+        <div className="shrink-0 flex-[0.75] border-t border-gray-200 lg:w-96 lg:border-l lg:border-t-0">
+          <ChatWrapper />
+        </div>
       </div>
     </div>
   );
---
+++ src/components/chat/ChatWrapper.tsx
@@ -0,0 +1,49 @@
+"use client";
+
+import { trpc } from "@/app/_trpc/client";
+import ChatInput from "./ChatInput";
+import Messages from "./Messages";
+import { Loader2 } from "lucide-react";
+
+interface ChatWrapperProps {
+  fileId: string;
+}
+
+const ChatWrapper = ({ fileId }: ChatWrapperProps) => {
+  const { data, isLoading } = trpc.getFileUploadStatus.useQuery(
+    {
+      fileId,
+    },
+    {
+      refetchInterval: (data) =>
+        data?.status === "SUCCESS" || data?.status === "FAILED" ? false : 500,
+    }
+  );
+
+  if (true)
+    return (
+      <div className="relative min-h-full bg-zinc-50 flex divide-y divide-zinc-200 flex-col justify-between gap-2">
+        <div className="flex-1 flex justify-center items-center flex-col mb-28">
+          <div className="flex flex-col items-center gap-2">
+            <Loader2 className="h-8 w-8 text-red-500 animate-spin"/>
+            <h3 className="font-semibold text-xl">Loading...</h3>
+            <p className="text-zinc-500 text-sm">
+              We&apos;re preparing your PDF.
+            </p>
+          </div>
+        </div>
+        <ChatInput />
+      </div>
+    );
+
+  return (
+    <div className="relative min-h-full bg-zinc-50 flex divide-y divide-zinc-200 flex-col justify-between gap-2">
+      <div>
+        <Messages />
+      </div>
+      <ChatInput isDisabled />
+    </div>
+  );
+};
+
+export default ChatWrapper;
