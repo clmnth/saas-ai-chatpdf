31d6020204efa1d093eb9601f100685006a8a50e
Clmt <huvigclement@gmail.com>
2023-12-14 17:37:14 +0700

    billing page and BillingForm

---
+++ src/app/dashboard/billing/page.tsx
@@ -0,0 +1,11 @@
+import BillingForm from "@/components/BillingForm";
+import { getUserSubscriptionPlan } from "@/lib/stripe";
+
+const Page = async () => {
+
+   const subscriptionPlan = await getUserSubscriptionPlan();
+
+  return <BillingForm subscriptionPlan={subscriptionPlan}/>
+};
+
+export default Page;
