5c5a368c0ff563c21d0a1f1640d7563b9a5c8687
Clmt <huvigclement@gmail.com>
2023-12-19 11:53:03 +0700

    add utility function for dynamic page metadata generation

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,14 +1,57 @@
-import { type ClassValue, clsx } from "clsx"
-import { type } from "os"
-import { twMerge } from "tailwind-merge"
- 
+import { type ClassValue, clsx } from "clsx";
+import { Metadata } from "next";
+import { twMerge } from "tailwind-merge";
+
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
 
 export function absoluteUrl(path: string) {
-  if(typeof window !== "undefined") return path
-  if(process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}${path}`
-  return `http://localhost:${process.env.PORT ?? 3000}${path}`
+  if (typeof window !== "undefined") return path;
+  if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}${path}`;
+  return `http://localhost:${process.env.PORT ?? 3000}${path}`;
 }
 
+export function constructMetadata({
+  title = "ChatPDF",
+  description = "ChatPDF is an open-source software to make chatting to your PDF files easy.",
+  image = "/thumbnail.png",
+  icons = "/favicon.ico",
+  noIndex = false,
+}: {
+  title?: string;
+  description?: string;
+  image?: string;
+  icons?: string;
+  noIndex?: boolean;
+} = {}): Metadata {
+  return {
+    title,
+    description,
+    openGraph: {
+      title,
+      description,
+      images: [
+        {
+          url: image,
+        },
+      ],
+    },
+    twitter: {
+      card: "summary_large_image",
+      title,
+      description,
+      images: [image],
+      creator: "@huvigclement",
+    },
+    icons,
+    metadataBase: new URL("http://localhost:3000/"),
+    themeColor: "#FFF",
+    ...(noIndex && {
+      robots: {
+        index: false,
+        follow: false,
+      },
+    }),
+  };
+}
